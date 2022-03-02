af3d05259d3200dc59b14c0bacea24341f7c661e
Matthew Ratzell <mjr4427@g.rit.edu>
2022-03-02 17:50:13 -0500

    Beautified what I could beutify given my time constraints

--- clientSrc/sw-card.js
+++ clientSrc/sw-card.js
@@ -3,23 +3,29 @@ template.innerHTML = `
 <style>  
 h2,h3
 {
-    
-    text-align: center;
     font-size: 2.0rem;
     margin-top: 0;
-    
+    color: #000080;
 }
 .card 
   {
     overflow:auto;
     padding:5%;
-    background-color: #E27D60 !important;
+    background-color: rgb(192, 189, 189) !important;
     height: 350px;
-    width: 306px;
+    width: 325px;
     margin-bottom:10px;
     justify-content:center;
   }
-  
+  .topOfCard{
+    margin-bottom:5%;
+  }
+  .middleOfCard{
+    margin-bottom:10%;
+  }
+  .bottomOfCard{
+  }
+
 .card:hover 
   {
     box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
@@ -31,12 +37,18 @@ h2,h3
 <body>
 
 <div class="card">
-
+  <div class="topOfCard"
     <h2 id="title" class="has-text-centered is-size-3">25</h2>
-    <hr>
+    </div>
+
+    <div class="middleOfCard"
     <h2 class="has-text-centered is-size-4" id="description">25</h2>
-    <hr>
-    <h2 id="dueDate" class="has-text-centered is-size-4">25</h2>
+    </div>
+    
+    <div class="bottomOfCard"
+    <h2 id="dueDate" class="has-text-centered is-size-5">25</h2>
+    </div>
+
 
 
 </div>
@@ -48,7 +60,7 @@ h2,h3
 class swCard extends HTMLElement {
   constructor() {
     super();
-    
+
     // attach a shadowdom treeto this instance this creasts a shadown root for us
     this.attachShadow({ mode: 'open' });// allows debugging when open
     // clone template and append it
@@ -56,7 +68,7 @@ class swCard extends HTMLElement {
   }
 
   connectedCallback() {
-    ondrop = function(){return false;};
+    ondrop = function () { return false; };
     // making sure we call our render function
     this.render();
   }
@@ -89,8 +101,8 @@ class swCard extends HTMLElement {
 
 
     this.titleDOM.innerHTML = `${title}`;
-    this.descriptionDOM.innerHTML = `Description:${description}`;
-    this.dueDateDOM.innerHTML = `Due Date:${dueDate}`;
+    this.descriptionDOM.innerHTML = `${description}`;
+    this.dueDateDOM.innerHTML = `Due:${dueDate}`;
 
   }
 }// end class
@@ -106,7 +118,7 @@ function allowDrop(ev) {
 function drop(ev) {
   ev.preventDefault();
   if (ev.target.className === 'clonedDiv' || ev.target.className === "card")
-      return;
+    return;
   var data = ev.dataTransfer.getData("text");
   //console.log(document.getElementById(data));
   ev.target.appendChild(document.getElementById(data));
